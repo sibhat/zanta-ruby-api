0ec5d6fb3292ed371b0b3a5d4f2f86110a5a848f
sibhat <sibhat.temelso@gmail.com>
2019-10-15 15:12:16 -0700

    created business and customers migration table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_205519) do
+ActiveRecord::Schema.define(version: 2019_10_10_210121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "business_name"
+    t.bigint "user_id"
+    t.string "location"
+    t.float "rating"
+    t.float "on_time_delivery"
+    t.float "order_completion"
+    t.decimal "last_month_earning"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_businesses_on_user_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_205519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "businesses", "users"
+  add_foreign_key "customers", "users"
 end
