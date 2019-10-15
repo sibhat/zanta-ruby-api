ecece52ce26bffc0899ad69ffd308b4a17b00388
sibhat <sibhat.temelso@gmail.com>
2019-10-15 15:11:12 -0700

    created namespace for businesses and customers route. It should also create scoped routes for devices to handle registration for users as well as customers. I chose this way for now, as I only worry about one db table that will handle authentication stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   devise_for :users, defaults: { format: :json }, controllers: {
     sessions: 'users/sessions', registrations: 'users/registrations'
-  }, path_names: {
+  }, path: 'api/v1', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'sign_up'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :businesses, only: %i[show destroy update]
+      resources :customers, only: %i[show destroy update]
+    end
+  end
   devise_scope :user do
-    post '/users/token', to: 'users/token#create'
+    post '/api/v1/token', to: 'users/token#create'
+    post '/api/v1/register/businesses' => 'users/token#create_business'
+    post '/api/v1/register/customers' => 'users/token#create_customer'
   end
 end
